565706fc8365cdabaff450cd328199ad346fa55e
omaranBazna <omran4d@gmail.com>
2022-06-03 08:31:35 -0400

    styling the nav

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react'
 import Product from "./Components/Product"
+import Logo from './graphics/logo.png'
 function App() { 
 const [lang,setLang]=useState('En')
   return (
@@ -15,10 +16,12 @@ const [lang,setLang]=useState('En')
        </div>
         */}
        <nav>
-        <img src="" className='logo' />
+        <img src={Logo} className='logo' />
+        <div className="nav-options">
         <a href="#" >about</a>
         <a href="#" >products</a>
         <a href="#" >contact us</a>
+        </div>
        </nav>
        <section  id="about">
        
