a126d6a5ccda4c2bc25887eceeff09e0a59c470b
omaranBazna <omran4d@gmail.com>
2022-06-02 23:21:19 -0400

    making the product component

---
+++ src/Components/Product.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Product(props){
+    return (
+        <div className="product">
+             <img src="" />
+            <h1>{this.props.title}</h1>
+            <h2>{this.props.describtion}</h2>
+        </div>
+    )
+}
