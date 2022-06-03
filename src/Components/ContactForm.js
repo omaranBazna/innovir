75f26fa69674e7f820bbc7626b4e642644e168a4
omaranBazna <omran4d@gmail.com>
2022-06-02 23:18:25 -0400

    making the sumbmit button

---
+++ src/Components/ContactForm.js
@@ -0,0 +1,19 @@
+import React from 'react'
+export default function contactForm(){
+   
+    return (
+
+        <div className="contact-form">
+             <form >
+                <input type="text" placeholder="Enter your name" />
+                <input type="text" placeholder="Enter your email" />
+                 <button >
+                     Submit
+                 </button>
+       
+
+             </form>
+
+        </div>
+    )
+}
